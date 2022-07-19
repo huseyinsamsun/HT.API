de5be800787effddda142e64dfba884edb367136
huseyinsamsunn <106733276+huseyinsamsunn@users.noreply.github.com>
2022-07-19 16:27:52 +0300

    Add project files.

---
+++ Core/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByName(string userName);
+        Task<Response<UserAppDto>> GetUserByEmail(string email);
+
+    }
+}
